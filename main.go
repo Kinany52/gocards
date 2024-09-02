59bb911109b10062261492e0226b95f7371ad4bb
Sarmad Al Kinany <sarmad.alkinany@tivian.com>
2024-09-02 12:33:54 +0200

    initial commit

---
+++ main.go
@@ -0,0 +1,12 @@
+// Package main is the entry point for the application.
+package main
+
+import "fmt"
+
+func main() {
+	//var card string = "Ace of Spades"
+	card := "Ace of Spades"
+	card = "Five of Diamonds"
+
+	fmt.Println(card)
+}
