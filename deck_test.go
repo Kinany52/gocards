4a3d1ce6cda255f9b19a5455b63cf077d68bc15e
Sarmad Al Kinany <draskinany@gmail.com>
2024-09-06 22:49:07 +0200

    more testing with Go

--- deck_test.go
+++ deck_test.go
@@ -9,4 +9,12 @@ func TestNewDeck(t *testing.T) {
 		// Use %v to format the integer value of len(d). %v is the formatting directive for integers.
 		t.Errorf("Expected deck length of 16, but got %v", len(d))
 	}
+
+	if d[0] != "Ace of Spades" {
+		t.Errorf("Expected first catd of Ace of Spades, but got %v", d[0])
+	}
+
+	if d[len(d)-1] != "Four of Clubs" {
+		t.Errorf("Expected first catd of Four of Clubs, but got %v", d[len(d)-1])
+	}
 }
